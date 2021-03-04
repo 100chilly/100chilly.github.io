7531403004a5ea7c9dfa56a39e3d50542bb756ec
Mitsue Tomohiro (100chilly) <100chilly@100chilly.website>
2021-03-03 20:17:28 -0500

    + Updated index.php

--- index.php
+++ index.php
@@ -386,48 +386,52 @@
 <!--Footer-->
 <<footer id="contact" class="page-footer scrollspy">
     <div class="container">  
-        <div class="row">
-            <div class="col l6 s12">
-                <form class="col s12" action="contact.php" method="post">
-                    <div class="row">
-                        <div class="input-field col s6">
-                            <i class="mdi-action-account-circle prefix white-text"></i>
-                            <input id="icon_prefix" name="name" type="text" class="validate white-text">
-                            <label for="icon_prefix" class="white-text">First Name</label>
-                        </div>
-                        <div class="input-field col s6">
-                            <i class="mdi-communication-email prefix white-text"></i>
-                            <input id="icon_email" name="email" type="email" class="validate white-text">
-                            <label for="icon_email" class="white-text">Email</label>
-                        </div>
-                        <div class="input-field col s12">
-                            <i class="mdi-editor-mode-edit prefix white-text"></i>
-                            <textarea id="icon_prefix2" name="message" class="materialize-textarea white-text"></textarea>
-                            <label for="icon_prefix2" class="white-text">Message</label>
-                        </div>
-                        <div class="col offset-s7 s5">
-                            <button class="btn waves-effect waves-light blue darken-1" type="submit">Submit
-                                <i class="mdi-content-send right white-text"></i>
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div>
+<!-- <div class="row">
             <div class="col l3 s12">
-                <h5 class="white-text"></h5>
-                <ul class="white-text">
-                </ul>
-            </div>
-            <div class="col l3 s12">
-                <h5 class="white-text">Favorite Quote:</h5>
-                <ul class="white-text">
-                    <li style="font-size: 190%; text-shadow: 2px 2px 4px;">"No. Try not. Do... or do not. There is no try."</li>
-                    <li style="font-size: 200%;">~Master Yoda</li>
-                </ul>
-            </div>
-            
-            </div>
-        </div>
+                    <h5 class="white-text"></h5>
+                    <ul class="white-text">
+                    </ul>
+                </div>
+                <div class="col l6 s12">
+                    <form class="col s12" action="contact.php" method="post">
+                        <div class="row">
+                            <div class="input-field col s6">
+                                <i class="mdi-action-account-circle prefix white-text"></i>
+                                <input id="icon_prefix" name="name" type="text" class="validate white-text">
+                                <label for="icon_prefix" class="white-text">First Name</label>
+                            </div>
+                            <div class="input-field col s6">
+                                <i class="mdi-communication-email prefix white-text"></i>
+                                <input id="icon_email" name="email" type="email" class="validate white-text">
+                                <label for="icon_email" class="white-text">Email</label>
+                            </div>
+                            <div class="input-field col s12">
+                                <i class="mdi-editor-mode-edit prefix white-text"></i>
+                                <textarea id="icon_prefix2" name="message" class="materialize-textarea white-text"></textarea>
+                                <label for="icon_prefix2" class="white-text">Message</label>
+                            </div>
+                            <div class="col offset-s7 s5">
+                                <button class="btn waves-effect waves-light blue darken-1" type="submit">Submit
+                                    <i class="mdi-content-send right white-text"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="col l3 s12">
+                    <h5 class="white-text"></h5>
+                    <ul class="white-text">
+                    </ul>
+                </div>
+                <div class="col l3 s12">
+                    <h5 class="white-text">Favorite Quote:</h5>
+                    <ul class="white-text">
+                        <li style="font-size: 190%; text-shadow: 2px 2px 4px;">"No. Try not. Do... or do not. There is no try."</li>
+                        <li style="font-size: 200%;">~Master Yoda</li>
+                    </ul>
+                </div>
+            </div> -->
+    </div>
     <div class="footer-copyright default_color">
         <div class="container">
             Made by <a class="importanttext" href="https://www.100chilly.website/">100chilly</a> (&copy; 100chilly)
